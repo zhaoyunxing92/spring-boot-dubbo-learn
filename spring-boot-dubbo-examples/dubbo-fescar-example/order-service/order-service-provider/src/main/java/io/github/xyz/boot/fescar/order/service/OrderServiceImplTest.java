de23cc780b507906d2f87cdc8cf152d87088d7ff
zhaoyunxing92 <2385585770@qq.com>
2019-03-22 16:08:15 +0800

    [up]调整作用域

---
+++ spring-boot-dubbo-examples/dubbo-fescar-example/order-service/order-service-provider/src/main/java/io/github/xyz/boot/fescar/order/service/OrderServiceImplTest.java
@@ -0,0 +1,15 @@
+package io.github.xyz.boot.fescar.order.service;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-03-22 15:55
+ * @des:
+ */
+public class OrderServiceImplTest extends JUnit4 {
+
+    @org.junit.Test
+    public void creatOrder() {
+    }
+}
