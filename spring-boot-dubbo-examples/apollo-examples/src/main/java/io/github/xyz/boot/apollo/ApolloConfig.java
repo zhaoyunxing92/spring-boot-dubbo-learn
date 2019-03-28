eec8aa13b6755430868d6ca5e002fc67a6272079
zhaoyunxing92 <2385585770@qq.com>
2019-03-28 20:59:46 +0800

    [up]修改文件位置

---
+++ spring-boot-dubbo-examples/apollo-examples/src/main/java/io/github/xyz/boot/apollo/ApolloConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.xyz.boot.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-03-28 20:44
+ * @des:
+ */
+@SpringBootApplication
+public class ApolloConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloConfig.class, args);
+    }
+}
